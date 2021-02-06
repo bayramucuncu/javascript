f51b3d04cf5e5762f245af8fb09710e2daff9d10
ucuncubayram <ucuncubayram@gmail.com>
2021-02-06 23:01:03 +0300

    Factorial algorithm complated.

---
+++ src/algorithms/factorial.js
@@ -0,0 +1,10 @@
+const factorial = (number) => {
+    let result = 1;
+
+    for (let i = 2; i <= number; i++) 
+        result = result * i;        
+
+    return result;
+}
+
+module.exports = factorial;
